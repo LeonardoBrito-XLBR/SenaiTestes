05f754f5c70eb5cec126f6c88f8920bd7a6a3335
Yatemaru <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2024-03-25 22:26:09 -0300

    Update and rename cobia 1 (4).c to Matrizes 2.c

--- Matrizes 2.c
+++ Matrizes 2.c
@@ -4,7 +4,7 @@
 	int main (){
 		setlocale(LC_ALL,"");
 		
-		//1º PRIME]~,~´~IRA LINHA ---- 2º SEGUNDO COLUNA
+		//1Âº PRIME]~,~Â´~IRA LINHA ---- 2Âº SEGUNDO COLUNA
 		char nome[2][200];
 		float notas[2][3];
 		int i, j; 
@@ -14,7 +14,7 @@
 			scanf("%s", &nome[i]);
 			
 			for (j = 0; j < 3; j ++){
-				printf ("Digite a %iª nota: ", j+1);
+				printf ("Digite a %iÂª nota: ", j+1);
 				scanf ("%f", &notas[i][j]);
 			}
 			
@@ -23,10 +23,10 @@
 		
 		printf ("\n=========================");
 		for ( i= 0; i <2; i++){
-			printf ("%iª Aluno: %s \n", i+1, nome[i]);
+			printf ("%iÂª Aluno: %s \n", i+1, nome[i]);
 			
 			for ( j= 0; j <3; j++){
-				printf ("%iª nota: %1.f \n", j+1, notas[i][j]);
+				printf ("%iÂª nota: %1.f \n", j+1, notas[i][j]);
 			}
 		}
 		
